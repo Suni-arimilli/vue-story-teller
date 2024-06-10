a8826661cd8f94d4f5f20ff1878cb5f3a4bcf0d5
Suni-arimilli <saisunisha.arimilli@eagles.oc.edu>
2024-06-10 16:55:52 -0400

    update some files in services folder for making api requests to backend

--- src/services/NarrativeServices.js
+++ src/services/NarrativeServices.js
@@ -3,5 +3,17 @@ import apiClient from "./services";
 export default {
   getAllNarratives() {
     return apiClient.get("narratives");
+  },
+  getNarrative(id) {
+    return apiClient.get(`narratives/${id}`);
+  },
+  createNarrative(narrative) {
+    return apiClient.post("narratives", narrative);
+  },
+  updateNarrative(narrative) {
+    return apiClient.put(`narratives/${narrative.id}`, narrative);
+  },
+  deleteNarrative(id) {
+    return apiClient.delete(`narratives/${id}`);
   }
 };
--- src/services/StoryCountryServices.js
+++ src/services/StoryCountryServices.js
@@ -3,5 +3,17 @@ import apiClient from "./services";
 export default {
   getAllCountries() {
     return apiClient.get("storyCountries");
+  },
+  getCountry(id) {
+    return apiClient.get(`storyCountries/${id}`);
+  },
+  createCountry(country) {
+    return apiClient.post("storyCountries", country);
+  },
+  updateCountry(country) {
+    return apiClient.put(`storyCountries/${country.id}`, country);
+  },
+  deleteCountry(id) {
+    return apiClient.delete(`storyCountries/${id}`);
   }
 };
